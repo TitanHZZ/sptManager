bb893e575589c4919c5955f67b05dcb606458113
TitanHZZ <titanhzz2018@gmail.com>
2022-07-01 16:22:54 +0100

    Added and configured the cmake project!

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Dev/c++/repos/sptManager
+# Build directory: C:/Dev/c++/repos/sptManager/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
